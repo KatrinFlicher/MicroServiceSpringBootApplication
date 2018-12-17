c48253865001b64f3c54b45cebf5d963ee26c476
Katsiaryna Zaretskaya <puperlastik@mail.ru>
2018-12-17 15:16:11 +0300

    Deleted all libraries

--- task2/src/by/training/katrinflicher/MetaSpaceOOMGenerator.java
+++ task2/src/by/training/katrinflicher/MetaSpaceOOMGenerator.java
@@ -7,23 +7,25 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class MetaSpaceOOMGenerator {
-        public static void generateError(){
-            Map<String, Comparable> myMap = new HashMap<>();
-            int iterations=0;
-            try{
-                while(true){
-                    String classLoaderJAR = "file:" +iterations+".jar";
-                    URL[] urlsOfJar = new URL[] {new URL(classLoaderJAR)};
-                    URLClassLoader aUrlClassLoader = new URLClassLoader(urlsOfJar);
-                    Comparable proxyObj = (Comparable) Proxy.newProxyInstance(aUrlClassLoader,
-                            new Class<?>[]{Comparable.class},
-                            (proxy,method,args)->{return method.invoke(proxy,args);});
-                    System.out.println(proxyObj.getClass());
-                    myMap.put(classLoaderJAR, proxyObj);
-                    iterations++;
-                }
-            }catch(Throwable anyError){
-                anyError.printStackTrace();
+    public static void generateError() {
+        Map<String, Comparable> myMap = new HashMap<>();
+        int iterations = 0;
+        try {
+            while (true) {
+                String classLoaderJAR = "file:" + iterations + ".jar";
+                URL[] urlsOfJar = new URL[]{new URL(classLoaderJAR)};
+                URLClassLoader aUrlClassLoader = new URLClassLoader(urlsOfJar);
+                Comparable proxyObj = (Comparable) Proxy.newProxyInstance(aUrlClassLoader,
+                        new Class<?>[]{Comparable.class},
+                        (proxy, method, args) -> {
+                            return method.invoke(proxy, args);
+                        });
+                System.out.println(proxyObj.getClass());
+                myMap.put(classLoaderJAR, proxyObj);
+                iterations++;
             }
+        } catch (Throwable anyError) {
+            anyError.printStackTrace();
         }
+    }
 }
