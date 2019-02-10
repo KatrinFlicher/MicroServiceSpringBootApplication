038244bb8c18179ff461edb1e8130b0b12ceed77
Katsiaryna Zaretskaya <puperlastik@mail.ru>
2019-02-11 00:42:18 +0300

    Add application.properties and Engine

---
+++ src/main/java/by/training/zaretskaya/TaskRestSpringApplication.java
@@ -0,0 +1,13 @@
+package by.training.zaretskaya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskRestSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskRestSpringApplication.class, args);
+    }
+}
+
