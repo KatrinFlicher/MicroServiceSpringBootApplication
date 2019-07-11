30364ff15eb5a39badf9ee2306f107fdbae56061
KatrinFlicher <katrinflicher@gmail.com>
2019-07-12 01:14:58 +0300

    Create new CollectionControllerTest

--- src/test/java/by/training/zaretskaya/controllers/CollectionControllerTest.java
+++ src/test/java/by/training/zaretskaya/controllers/CollectionControllerTest.java
@@ -7,143 +7,92 @@ import by.training.zaretskaya.services.ICollectionService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
-import org.skyscreamer.jsonassert.JSONAssert;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.RequestBuilder;
-import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import java.util.Arrays;
 import java.util.List;
 
-import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-//@RunWith(SpringRunner.class)
-//@WebMvcTest(value = CollectionController.class, secure = false)
+@RunWith(SpringRunner.class)
+@WebMvcTest(value = CollectionController.class, secure = false)
 public class CollectionControllerTest {
+    private final Collection mockCollection = new Collection("cats", 12, "LFU",
+            "{\n" +
+                    "  \"type\": \"string\",\n" +
+                    "  \"minLength\": 2,\n" +
+                    "  \"maxLength\": 3\n" +
+                    "}");
+    private final String exampleCollectionJson = "{\"name\":\"cats\",\"cacheAlgorithm\":\"LFU\"," +
+            "\"cacheLimit\":12,\"jsonScheme\":\"{\\n  \\\"type\\\": \\\"string\\\",\\n  " +
+            "\\\"minLength\\\": 2,\\n  \\\"maxLength\\\": 3\\n}\"}";
+    @MockBean
+    DistributedCollectionService<Collection> distributedService;
+    @MockBean
+    Node node;
+    @Autowired
+    private MockMvc mockMvc;
+    @MockBean
+    @Qualifier("CollectionService")
+    private ICollectionService<Collection> collectionService;
 
-//    private Collection mockCollection = new Collection("cats", 12, "LFU", "{\n" +
-//            "  \"type\": \"string\",\n" +
-//            "  \"minLength\": 2,\n" +
-//            "  \"maxLength\": 3\n" +
-//            "}");
-//    private String exampleCollectionJson = " {\n" +
-//            "        \"name\": \"cats\",\n" +
-//            "        \"cacheLimit\": 12,\n" +
-//            "        \"cacheAlgorithm\": \"LFU\"\n" +
-//            "        \"jsonScheme\": {\n" +
-//            "  \"type\": \"string\",\n" +
-//            "  \"minLength\": 2,\n" +
-//            "  \"maxLength\": 30\n} \n}";
-//
-//    String jo = "{\n" +
-//            "  \"type\": \"string\",\n" +
-//            "  \"minLength\": 2,\n" +
-//            "  \"maxLength\": 30\n" +
-//            "}";
-//
-//    @Autowired
-//    private MockMvc mockMvc;
-//    @MockBean
-//    private ICollectionService collectionService;
-//    @MockBean
-//    DistributedCollectionService distributedService;
-//    @MockBean
-//    Node node;
-//
-//
-//
-//
-//    @Test
-//    public void testCreateCollection() throws Exception {
-//        RequestBuilder requestBuilder = MockMvcRequestBuilders
-//                .post("/rest")
-//                .accept(MediaType.APPLICATION_JSON)
-//                .content(exampleCollectionJson)
-//                .contentType(MediaType.APPLICATION_JSON);
-//        when(distributedService.isMyGroup(Mockito.anyString())).thenReturn(true);
-//        MvcResult result = mockMvc.perform(requestBuilder).andReturn();
-//        MockHttpServletResponse response = result.getResponse();
-//        Mockito.verify(collectionService).create(Mockito.any(Collection.class));
-//        assertEquals(HttpStatus.CREATED.value(), response.getStatus());
-//        assertEquals("http://localhost/rest/cats",
-//                response.getHeader(HttpHeaders.LOCATION));
-//    }
-//
-//    @Test
-//    public void testListCollections() throws Exception {
-//        String expected = "[" + exampleCollectionJson + "," + exampleCollectionJson + "," + exampleCollectionJson + "]";
-//        List<Collection> collections = Arrays.asList(mockCollection, mockCollection, mockCollection);
-//        when(collectionService
-//                .listCollections(Mockito.anyInt(), Mockito.anyInt()))
-//                .thenReturn(collections);
-//        RequestBuilder requestBuilder = MockMvcRequestBuilders
-//                .get("/rest")
-//                .accept(MediaType.APPLICATION_JSON);
-//        MvcResult result = mockMvc.perform(requestBuilder).andReturn();
-//        JSONAssert.assertEquals(expected, result.getResponse().getContentAsString(), false);
-//    }
-//
-//    @Test
-//    public void testGetCollectionById() throws Exception {
-//        when(
-//                collectionService.getById(Mockito.anyString())).thenReturn(mockCollection);
-//        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.get(
-//                "/rest/cats").accept(MediaType.APPLICATION_JSON);
-//        MvcResult result = mockMvc.perform(requestBuilder).andReturn();
-//        JSONAssert.assertEquals(exampleCollectionJson, result.getResponse().getContentAsString(), false);
-//    }
-//
-//    @Test
-//    public void testDeleteCollection() throws Exception {
-//        RequestBuilder requestBuilder = MockMvcRequestBuilders
-//                .delete("/rest/doctors")
-//                .accept(MediaType.APPLICATION_JSON);
-//        MvcResult result = mockMvc.perform(requestBuilder).andReturn();
-//        Mockito.verify(collectionService).delete(Mockito.anyString());
-//        assertEquals(HttpStatus.NO_CONTENT.value(), result.getResponse().getStatus());
-//    }
-//
-//    @Test
-//    public void testUpdateCollectionName() throws Exception {
-//        MvcResult result = prepareUpdate("name");
-//        Mockito.verify(collectionService)
-//                .updateName(Mockito.anyString(), Mockito.anyString());
-//        assertEquals(HttpStatus.NO_CONTENT.value(), result.getResponse().getStatus());
-//    }
-//
-//    @Test
-//    public void testUpdateCollectionCacheLimit() throws Exception {
-//        MvcResult result = prepareUpdate("limit");
-//        Mockito.verify(collectionService)
-//                .updateCacheLimit(Mockito.anyString(), Mockito.anyInt());
-//        assertEquals(HttpStatus.NO_CONTENT.value(), result.getResponse().getStatus());
-//    }
-//
-//    @Test
-//    public void testUpdateCollectionAlgorithm() throws Exception {
-//        MvcResult result = prepareUpdate("cacheAlgorithm");
-//        Mockito.verify(collectionService)
-//                .update(Mockito.anyString(), Mockito.anyString());
-//        assertEquals(HttpStatus.NO_CONTENT.value(), result.getResponse().getStatus());
-//    }
-//
-//    private MvcResult prepareUpdate(String changeableValue) throws Exception {
-//        RequestBuilder requestBuilder = MockMvcRequestBuilders
-//                .put("/rest/cats/" + changeableValue)
-//                .accept(MediaType.APPLICATION_JSON)
-//                .content(exampleCollectionJson)
-//                .contentType(MediaType.APPLICATION_JSON);
-//        return mockMvc.perform(requestBuilder).andReturn();
-//    }
+    @Test
+    public void getCollectionById() throws Exception {
+        when(collectionService.getById(Mockito.anyString())).thenReturn(mockCollection);
+        mockMvc.perform(get("/rest/" + mockCollection.getName())
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk()).andExpect(content().json(exampleCollectionJson));
+    }
+
+    @Test
+    public void createCollection() throws Exception {
+        mockMvc.perform(post("/rest")
+                .accept(MediaType.APPLICATION_JSON)
+                .content(exampleCollectionJson)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated())
+                .andExpect(header().string("Location", "http://localhost/rest/cats"));
+        Mockito.verify(collectionService).create(Mockito.any(Collection.class));
+        Mockito.verify(distributedService).create(Mockito.any(Collection.class));
+    }
+
+    @Test
+    public void updateCollection() throws Exception {
+        mockMvc.perform(put("/rest/" + mockCollection.getName())
+                .accept(MediaType.APPLICATION_JSON)
+                .content(exampleCollectionJson)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNoContent());
+        Mockito.verify(collectionService).update(Mockito.anyString(), Mockito.any(Collection.class));
+        Mockito.verify(distributedService).update(Mockito.anyString(), Mockito.any(Collection.class));
+    }
+
+    @Test
+    public void deleteCollection() throws Exception {
+        mockMvc.perform(delete("/rest/" + mockCollection.getName())
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNoContent());
+        Mockito.verify(collectionService).delete(Mockito.anyString());
+        Mockito.verify(distributedService).delete(Mockito.anyString());
+    }
+
+    @Test
+    public void listCollections() throws Exception {
+        String expectedList = "[" + exampleCollectionJson + "," + exampleCollectionJson +
+                "," + exampleCollectionJson + "]";
+        List<Collection> collections = Arrays.asList(mockCollection, mockCollection, mockCollection);
+        when(collectionService.listCollections("", 4))
+                .thenReturn(collections);
+        mockMvc.perform(get("/rest")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(content().json(expectedList));
+    }
 }
